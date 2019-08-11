384fd799a81dbddf372d1e6caf8bd6a8498ddb03
rwywawn <rwywawnw@gmail.com>
2019-08-11 15:06:09 -0400

    Edit code

--- Files/index.js
+++ Files/index.js
@@ -1,9 +1,14 @@
-alert("smtg");
-alert("wait");
-lost = true; 
-if (lost === true) {
-    alert("lol");
-}
-else {
-    alert('not lol');
-}
+
+let apiKey = "J0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjBlNzZiNzVhLWRlNjYtNGViZi05YzI1LTBlNGIyNDY0MDliNiIsImlhdCI6MTU2NTMwMTE1OCwic3ViIjoiZGV2ZWxvcGVyL2NiMDgxZjg3LTdiY2ItOTA4OC0zNTRlLWFkNDg5MjY0NzhkZSIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI3MC41MC4xNS4yNCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.UBiBAigBoXWxBuiSlgo0y5GHgjX3WlZHK5CaNYuVT5PqBBUkuYNAawLwCkIodbzae7A-bVPNBjGb0Dc00ziQrA";
+testEndpoint = '/clan/#LGG99U0';
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var request = new XMLHttpRequest();
+
+const url = 'https://api.clashroyale.com/v1';
+
+endpoint = `${url}${testEndpoint}${apiKey}`;
+request.open('GET', endpoint, true);
+
+document.getElementById('word').innerHTML = response.json();
+
+request.send()
