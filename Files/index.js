4cc94dab5d2a207f36ac2edbe72b0838b27cce1c
rwywawn <rwywawnw@gmail.com>
2019-08-18 22:53:42 -0400

    Make into a website

--- Files/index.js
+++ Files/index.js
@@ -1,32 +1,69 @@
+//calling api
 
-const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjBlNzZiNzVhLWRlNjYtNGViZi05YzI1LTBlNGIyNDY0MDliNiIsImlhdCI6MTU2NTMwMTE1OCwic3ViIjoiZGV2ZWxvcGVyL2NiMDgxZjg3LTdiY2ItOTA4OC0zNTRlLWFkNDg5MjY0NzhkZSIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI3MC41MC4xNS4yNCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.UBiBAigBoXWxBuiSlgo0y5GHgjX3WlZHK5CaNYuVT5PqBBUkuYNAawLwCkIodbzae7A-bVPNBjGb0Dc00ziQrA";
-const testEndpoint = '/clans/%23LGG99U0/members';
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+var token = require('../.gitignore/token.js');
 
-const url = 'https://api.clashroyale.com/v1';
-
-const point = url+testEndpoint;
-
-const callApi = (endpoint, apiKey) => {
+const start = () => {
+     
+    const testEndpoint = '/clans/%23LGG99U0/members';
+   
+    const url = 'https://api.clashroyale.com/v1';
     
-    var request = new XMLHttpRequest();
-    request.open('GET', endpoint);
-    request.setRequestHeader('authorization', 'Bearer ' + apiKey)
-    request.setRequestHeader('Accept', `application/json`)
-    request.addEventListener('load', read)
-    function read(){
+    const point = url + testEndpoint;
+    const callApi = (endpoint, apiKey) => {
+        update.innerHTML = "loading";
+        var request = new XMLHttpRequest();
+        
+        request.open('GET', endpoint,true);
+        request.setRequestHeader('authorization', 'Bearer ' + apiKey)
+        request.setRequestHeader('Content-Type', `application/json`)
+        request.addEventListener('load', read)
+        
+        
+        function read() {
 
-        if (this.status === 200) {
-
-            console.log(JSON.parse(this.responseText));
-        } else {
-            console.log(this.status);
-            console.log(this.responseText);
+            if (this.status === 200) {
+                
+                const parsed = (JSON.parse(this.responseText));
+                console.log(parsed);
+                const members = parsed.items.map(current => current.name);
+                console.log(members);
+                update.innerHTML = "Done";
+                makeTable(parsed.items)
+            } else {
+                console.log('Error ' + this.status);
+                console.log(this.responseText);
+                update.innerHTML = "Error";
+            }
         }
+        request.send()
     }
-    request.send()
-
+    callApi(point, token());
 }
-console.log(point);
-console.log(token);
-callApi(point, token);
+const makeTable = (array) => {
+    var table = document.getElementById("table"); 
+    array.forEach(function (member) {
+        var row = document.createElement("tr");
+        Object.entries(member).forEach(function (key) {
+            
+            if ((key[0] !== "clanRank" )&&( key[0] !== "previousClanRank") &&(key[0] !=="clanChestPoints")) {
+                if (key[0] === "arena") {
+                    
+                    key[1] = key[1].name;
+                }
+                var cell = document.createElement("td");
+                cell.textContent = key[1];
+                row.appendChild(cell);
+            } 
+            
+        });
+        table.appendChild(row);
+    });
+}
+// website
+let update = document.getElementById("word");
+
+update.onclick = () => {
+    update.innerHTML = "clicked";
+    start();
+}
