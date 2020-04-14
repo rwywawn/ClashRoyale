2738f52573f290780e382b2f1dda31a880e0a846
Adrien Pringle <adrienpringle@gmail.com>
2020-04-14 23:33:20 +0800

    Improved table

---
+++ Frontend/debug.js
@@ -0,0 +1,6 @@
+//debuging purposes - remove for real thing
+
+window.onload = function(){
+  document.getElementById('clanTag').value = "LGG99U0";
+  document.getElementById('submit').click();
+}
--- Frontend/index.js
+++ Frontend/index.js
@@ -1,4 +1,3 @@
-
 const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 const token = require('../.gitignore/token.js');
 
@@ -7,10 +6,10 @@ const text = document.getElementById('info');
 
 
 const makeTable = (request) => {
-    
+
     const parsed = JSON.parse(request).items
     const table = document.getElementById("tableBody");
-   
+
     if (table.rows.length > 0) {
         for (let x = table.rows.length-1 ; x >= 0; x--) {
             table.deleteRow(x);
@@ -23,14 +22,33 @@ const makeTable = (request) => {
                 if (key[0] === "arena") {
                     key[1] = key[1].name;
                 }
+                if (key[0] === "lastSeen"){
+                    key[1] = parseDate(key[1]);
+                }
                 const cell = document.createElement("td");
                 cell.textContent = key[1];
+
+                if (key[0] === "expLevel"){
+                  var img = document.createElement("img");
+                  img.src = "../frontend/experience-levels/"+key[1]+".png";
+                  img.classList.add("exp");
+                  cell.textContent = '';
+                  cell.appendChild(img);
+                }
+
                 row.appendChild(cell);
             }
         });
         table.appendChild(row);
     });
     text.innerHTML = "Done";
+}
+function parseDate(date){
+    d = date.replace('T', '').match(/.{2}/g);
+    //return ''+d[0]+d[1] + '-' + d[2] + '-' + d[3] + ' ' + d[4] + ':' + d[5];
+    var options = {month: 'short', day: 'numeric', hour:'2-digit', minute: '2-digit', hour12: false };
+    da = new Date(''+d[0]+d[1] + '-' + d[2] + '-' + d[3] + 'T' + d[4] + ':' + d[5]+':'+d[6]+".000z");
+    return da.toLocaleDateString("en-US", options);
 }
 
 // website
@@ -39,35 +57,35 @@ submit.onclick = async () => {
     const clanReq = {
         "clanTag": clanTag
     }
-   
+
     const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
     const request = new XMLHttpRequest();
-    
+
     const callApi = new Promise(function(resolve, reject) {
         request.open('POST', "http://localhost:3000/api/clans", true);
         request.setRequestHeader('Content-Type', `application/json`);
         request.onload = function () {
             if (this.status === 200) {
-                
+
                 if (JSON.parse(this.responseText).status === 200) {
-                   
+
                     resolve(JSON.parse(this.responseText).responseText)
                 } else {
-                   
+
                     reject(JSON.parse(this.responseText))
                 }
-            
+
             } else {
                 reject({ status: this.status, text: this.responseText })
             }
         }
         request.send(JSON.stringify(clanReq));
     })
-    
+
     callApi.then(function (req) {
         makeTable(req);
     })
     .catch(function (error) {
-        text.innerHTML = ('Error ' + error.status + " " + error.responseText); 
+        text.innerHTML = ('Error ' + error.status + " " + error.responseText);
     })
 }
